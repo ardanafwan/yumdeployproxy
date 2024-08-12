65632d04df61e8dba28c09c22d58768b5041bb27
ardan afwan <ardanafwan@gmail.com>
2024-08-12 14:57:45 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x3e896F05Fa0DbdCBB0A7860Cb45E2DC5A14D1942'
+export const ProxyAdmin = '0x3E6EFA45019972685e2FC775504E3F01Fc69b5e2'
+export const SWTRImplementation = '0x3d7880D8975a05B616092ec99597CA08d5C31134'
     
